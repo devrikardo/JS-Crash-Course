6c7095591b7552f69b2f1afc7b716c975668d4a8
devrikardo <andreasrikardo1@gmail.com>
2020-06-10 11:09:23 +0700

    js-tutorial-1

---
+++ vendor.js
@@ -0,0 +1,13 @@
+const userInput = document.getElementById("input-number");
+const addBtn = document.getElementById("btn-add");
+const substactBtn = document.getElementById("btn-substact");
+const multiplyBtn = document.getElementById("btn-multiply");
+const divideBtn = document.getElementById("btn-divide");
+
+const currentResultOutput = document.getElementById("current-result");
+const currentCalculationOutput = document.getElementById("current-calculation");
+
+function outputResult(result, text) {
+  currentResultOutput.textContent = result;
+  currentCalculationOutput.textContent = text;
+}
